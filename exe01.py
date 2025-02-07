776d5c0d50018827fd66905fbd4071a0a961d1b0
maxmanoel <max329900@gmail.com>
2025-02-06 21:26:23 -0300

    alteracaodoexe01
    exe01

--- exe01.py
+++ exe01.py
@@ -1,4 +1,8 @@
-# max manoel
-# 16 anos 
-#gosto de anda de bike 
-# eu amo programacao 
+x = 34 - 23  #um comentario
+y = "Hello" #outro comentario
+z = 3.45
+if z == 3.45 or y == "Hello":
+    x = x + 1
+    y = y + "Word" #concatenção de strings
+    print(x)
+    print(y)
