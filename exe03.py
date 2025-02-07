a030078e82367152e434c2875867ece25c3961b7
maxmanoel <max329900@gmail.com>
2025-02-06 22:00:06 -0300

    alteracaoexe03
    alteracao nas atividades

---
+++ exe03.py
@@ -0,0 +1,6 @@
+# Faça um programa que receba um salario de 1500 e tenha um aumento de 55 do salario e ao final e exiba o salario com o aumento
+print ("Voce recebeu 1500 reais")
+x = 1500
+z = x * 0.05
+x = x + z
+print(x)
---
+++ exez02.py
@@ -0,0 +1,3 @@
+a = 2 #Variavl a receber o valor2;
+b = 3 # Variavel b receber o valor 3;
+print (a + b) # print e uma função que exibe a soma de a + b;
