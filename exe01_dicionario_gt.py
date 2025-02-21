be35aae0ec31ceb761dd1dce54b2e71b8a9c1374
maxmanoel <max329900@gmail.com>
2025-02-21 19:04:08 -0300

    alteracao

---
+++ exe01_dicionario_gt.py
@@ -0,0 +1,7 @@
+mais_vendidos = ("tecnologia ", "iphone", "refrigerante", "ar consul 1200 btu", "livros " , "alquimista", "eletrodomesticos", "geladeira")
+
+vendas_tecnologia = ({"iphone":1500, "sansng galaxy":1200, "tv sansung:"1000 : "ps5":14300, "tablet":1720, "ipad":1200})
+livro = mais_vendidos ("livros")
+print(mais_vendidos.get("livros")
+print(mais_vendidos["livros"]))
+print("fim programa")
---
+++ exe03_dicionario_adiconar.py
@@ -0,0 +1,7 @@
+venda_vendedores = {}
+venda_vendedores["joao"] = 15000
+print(venda_vendedores)
+
+venda_vendedores.update({"Lira":50, "Nathalie" : 100})
+print(venda_vendedores)
+print("fim programa")
